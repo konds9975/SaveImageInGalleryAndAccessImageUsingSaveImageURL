104f0c57b7bcc5ed2cf51e038891bb2aa77f714d
Hitendra Bhoir <hitendra@hitendras-mac-mini.local>
2018-06-26 16:54:40 +0530

    IntiaLCOMMIT

---
+++ SaveImageInGalleryAndAccessImageUsingSaveImageURL/File.swift
@@ -0,0 +1,136 @@
+import UIKit
+import Photos
+
+extension PHPhotoLibrary {
+    
+    
+    func getURL(ofPhotoWith mPhasset: PHAsset, completionHandler : @escaping ((_ responseURL : URL?) -> Void)) {
+    
+    if mPhasset.mediaType == .image {
+        let options: PHContentEditingInputRequestOptions = PHContentEditingInputRequestOptions()
+        options.canHandleAdjustmentData = {(adjustmeta: PHAdjustmentData) -> Bool in
+            return true
+        }
+        mPhasset.requestContentEditingInput(with: options, completionHandler: { (contentEditingInput, info) in
+            completionHandler(contentEditingInput!.fullSizeImageURL)
+        })
+    } else if mPhasset.mediaType == .video {
+        let options: PHVideoRequestOptions = PHVideoRequestOptions()
+        options.version = .original
+        PHImageManager.default().requestAVAsset(forVideo: mPhasset, options: options, resultHandler: { (asset, audioMix, info) in
+            if let urlAsset = asset as? AVURLAsset {
+                let localVideoUrl = urlAsset.url
+                completionHandler(localVideoUrl)
+            } else {
+                completionHandler(nil)
+            }
+        })
+    }
+    
+    }
+    
+    
+    
+    
+    
+    // MARK: - PHPhotoLibrary+SaveImage
+    
+    
+    
+    
+    func fetchAssetCollectionForAlbum(name:String) -> PHAssetCollection! {
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.predicate = NSPredicate(format: "title = %@",name)
+        let collection = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: fetchOptions)
+        
+        if let _: AnyObject = collection.firstObject {
+            return collection.firstObject
+        }
+       return nil
+    }
+    // MARK: - Public
+    func savePhoto(image:UIImage, albumName:String, completion:((PHAsset?)->())? = nil) {
+        func save() {
+            if let album = PHPhotoLibrary.shared().findAlbum(albumName: albumName) {
+                PHPhotoLibrary.shared().saveImage(image: image, album: album, completion: completion)
+            } else {
+                PHPhotoLibrary.shared().createAlbum(albumName: albumName, completion: { (collection) in
+                    if let collection = collection {
+                        PHPhotoLibrary.shared().saveImage(image: image, album: collection, completion: completion)
+                    } else {
+                        completion?(nil)
+                    }
+                })
+            }
+        }
+        if PHPhotoLibrary.authorizationStatus() == .authorized {
+            save()
+        } else {
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                if status == .authorized {
+                    save()
+                }
+            })
+        }
+    }
+    
+    // MARK: - Private
+    
+    fileprivate func findAlbum(albumName: String) -> PHAssetCollection? {
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.predicate = NSPredicate(format: "title = %@", albumName)
+        let fetchResult : PHFetchResult = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: fetchOptions)
+        guard let photoAlbum = fetchResult.firstObject else {
+            return nil
+        }
+        return photoAlbum
+    }
+    
+    fileprivate func createAlbum(albumName: String, completion: @escaping (PHAssetCollection?)->()) {
+        var albumPlaceholder: PHObjectPlaceholder?
+        PHPhotoLibrary.shared().performChanges({
+            let createAlbumRequest = PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: albumName)
+            albumPlaceholder = createAlbumRequest.placeholderForCreatedAssetCollection
+        }, completionHandler: { success, error in
+            if success {
+                guard let placeholder = albumPlaceholder else {
+                    completion(nil)
+                    return
+                }
+                let fetchResult = PHAssetCollection.fetchAssetCollections(withLocalIdentifiers: [placeholder.localIdentifier], options: nil)
+                guard let album = fetchResult.firstObject else {
+                    completion(nil)
+                    return
+                }
+                completion(album)
+            } else {
+                completion(nil)
+            }
+        })
+    }
+    
+    fileprivate func saveImage(image: UIImage, album: PHAssetCollection, completion:((PHAsset?)->())? = nil) {
+        var placeholder: PHObjectPlaceholder?
+        PHPhotoLibrary.shared().performChanges({
+            let createAssetRequest = PHAssetChangeRequest.creationRequestForAsset(from: image)
+            guard let albumChangeRequest = PHAssetCollectionChangeRequest(for: album),
+                let photoPlaceholder = createAssetRequest.placeholderForCreatedAsset else { return }
+            placeholder = photoPlaceholder
+            let fastEnumeration = NSArray(array: [photoPlaceholder] as [PHObjectPlaceholder])
+            albumChangeRequest.addAssets(fastEnumeration)
+        }, completionHandler: { success, error in
+            guard let placeholder = placeholder else {
+                completion?(nil)
+                return
+            }
+            if success {
+                let assets:PHFetchResult<PHAsset> =  PHAsset.fetchAssets(withLocalIdentifiers: [placeholder.localIdentifier], options: nil)
+                let asset:PHAsset? = assets.firstObject
+                completion?(asset)
+            } else {
+                completion?(nil)
+            }
+        })
+    }
+}
--- SaveImageInGalleryAndAccessImageUsingSaveImageURL/ViewController.swift
+++ SaveImageInGalleryAndAccessImageUsingSaveImageURL/ViewController.swift
@@ -1,25 +1,90 @@
 //
 //  ViewController.swift
-//  SaveImageInGalleryAndAccessImageUsingSaveImageURL
+//  SelfieTime
 //
-//  Created by Hitendra Bhoir on 26/06/18.
-//  Copyright © 2018 Hitendra Bhoir. All rights reserved.
+//  Created by Andrew Seeley on 11/09/2016.
+//  Copyright © 2016 Seemu. All rights reserved.
 //
 
 import UIKit
+import Photos
+import AssetsLibrary
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var image3: UIImageView!
+    @IBOutlet var myImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(PHPhotoLibrary.shared().fetchAssetCollectionForAlbum(name: "Fabbit"))
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func takePhoto(_ sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myImg.contentMode = .scaleToFill
+            myImg.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func savePhoto(_ sender: AnyObject) {
+        
+        
+        
+        //        ALAssetsLibrary().writeImage(toSavedPhotosAlbum: myImg.image?.cgImage, orientation: ALAssetOrientation(rawValue: myImg.image!.imageOrientation.rawValue)!,
+        //                                                       completionBlock:{ (path:URL!, error:Error!) -> Void in
+        //                                                        print("\(path)")
+        //        })
+        PHPhotoLibrary.shared().savePhoto(image: myImg.image!, albumName: "Fabbit"){ (image)in
+            
+            print(PHPhotoLibrary.shared().getURL(ofPhotoWith: image!, completionHandler: { (url) in
+                print(url ?? "nn")
+                
+                do{
+                    let catPictureData = try Data(contentsOf: url!)
+                    let catPicture = UIImage(data: catPictureData )
+                    self.image3.image = catPicture
+                }catch
+                {
+                    print(error)
+                }
+                
+            }))
+        }
+        
+        //        let imageData = UIImagePNGRepresentation(myImg.image!)
+        //        let compresedImage = UIImage(data: imageData!)
+        //        UIImageWriteToSavedPhotosAlbum(compresedImage!, nil, nil, nil)
+        
+        let alert = UIAlertController(title: "Saved", message: "Your image has been saved", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+        
+        
+        
+        
+        
+    }
+    
+    
 }
 
